b726018f4b6116bda97d19c9eba733e2a4525b57
Fabian Helm <fabian.helm@abacus.ac>
2018-12-27 17:16:10 +0100

    fix middleware

--- heatingcontrol/middleware.py
+++ heatingcontrol/middleware.py
@@ -1,8 +1,12 @@
 from django.contrib.auth import authenticate, login
 
 class MobileClientLoginMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
     def __call__(self, request):
-#        print(request.META['REMOTE_HOST'])
+        response = self.get_response(request)
+#       print(request.META['REMOTE_HOST'])
 
         if request.path.find('admin') == -1 and not request.user.is_authenticated():
             if request.GET.get('mobile', None) == 'authenticate':
@@ -10,3 +14,4 @@ class MobileClientLoginMiddleware:
                 if user and user.is_active:
                     login(request, user)
 
+        return response
--- heatingcontrol/settings.py
+++ heatingcontrol/settings.py
@@ -40,17 +40,17 @@ INSTALLED_APPS = (
     'heatingcontrol'
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'heatingcontrol.middleware.MobileClientLoginMiddleware',
-)
+]
 
 ROOT_URLCONF = 'heatingcontrol.urls'
 
