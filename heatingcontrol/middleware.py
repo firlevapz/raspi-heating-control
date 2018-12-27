bdf813e5c7fbfbf2ef11e4ef45c23be0c6eb1f5b
Fabian Helm <fabian.helm@abacus.ac>
2018-12-27 17:17:21 +0100

    fix is_authenticated

--- heatingcontrol/middleware.py
+++ heatingcontrol/middleware.py
@@ -8,7 +8,7 @@ class MobileClientLoginMiddleware:
         response = self.get_response(request)
 #       print(request.META['REMOTE_HOST'])
 
-        if request.path.find('admin') == -1 and not request.user.is_authenticated():
+        if request.path.find('admin') == -1 and not request.user.is_authenticated:
             if request.GET.get('mobile', None) == 'authenticate':
                 user = authenticate(username='mobile', password='mobile')
                 if user and user.is_active:
